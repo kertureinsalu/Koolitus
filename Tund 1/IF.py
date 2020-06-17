8ce8891be03d3f36c9ce18c385a6ca214d4f0625
kertureinsalu <67063933+kertureinsalu@users.noreply.github.com>
2020-06-17 18:27:57 +0300

    Add files via upload

---
+++ Tund 1/IF.py
@@ -0,0 +1,6 @@
+sisestusInt1 = int(input("sisesta esimene number"))
+sisestusInt2 = int(input("sisesta teine number"))
+if sisestusInt1 > sisestusInt2:
+    print ("ongi suurem")
+if sisestusInt1 < sisestusInt2:
+    print ("ongi väiksem")
---
+++ Tund 1/Lihtne kalkulaator.py
@@ -0,0 +1,4 @@
+sisestusInt1 = int(input("Sisesta esimene number"))
+sisestusInt2 =int(input("Sisesta teine number"))
+liitmine = sisestusInt1 + sisestusInt2
+print (liitmine)
